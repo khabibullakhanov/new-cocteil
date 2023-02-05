8c0acce4a9714ab1161fbe0cb67ed4f3168457e9
macstore.uz <macstore.uz@macstoreuzui-MacBookAir.local>
2023-02-05 23:17:01 +0500

    fixix add fedback

--- src/Companents/AllFeedbackCompanent/AllFeedbackCompanent.js
+++ src/Companents/AllFeedbackCompanent/AllFeedbackCompanent.js
@@ -10,43 +10,46 @@ export function AllFeedbackCompanent() {
   console.log(allFeedbacks.date);
 
   return (
-    <div id="feedbacks-companents-container">
-      <div id="feedbacks-companents-container-top">
-        <p>{allFeedbacks.length} отзывов</p>
-        <div>
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarOutlinedIcon />
+    <div id="feedbacks-main-companents-container">
+      <div id="feedbacks-companents-container">
+        <div id="feedbacks-companents-container-top">
+          <p>{allFeedbacks.length} отзывов</p>
+          <div>
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+            <StarOutlinedIcon />
+          </div>
         </div>
-      </div>
-      {allFeedbacks.map((item, index) => {
-        return (
-          <div id="feedback-content-mainy" key={index}>
-            <figure id="feedback-content-mainy-figure">
-              <img src={MyImage} alt="" />
-            </figure>
-            <div id="feedback-content-mainy-right-content">
-              <div id="feedback-content-mainy-right-content-top-content">
-                <div>
-                  <StarOutlinedIcon />
-                  <StarOutlinedIcon />
-                  <StarOutlinedIcon />
-                  <StarOutlinedIcon />
-                  <StarOutlinedIcon />
+        {allFeedbacks.map((item, index) => {
+          return (
+            <div id="feedback-content-mainy" key={index}>
+              <figure id="feedback-content-mainy-figure">
+                <img src={MyImage} alt="" />
+              </figure>
+              <div id="feedback-content-mainy-right-content">
+                <div id="feedback-content-mainy-right-content-top-content">
+                  <div>
+                    <StarOutlinedIcon />
+                    <StarOutlinedIcon />
+                    <StarOutlinedIcon />
+                    <StarOutlinedIcon />
+                    <StarOutlinedIcon />
+                  </div>
+                  <p>{item.name}</p>
                 </div>
-                <p>{item.name}</p>
-              </div>
-              <span>{item.feedback}</span>
-              <div id="feedback-content-mainy-right-content-bottom-content">
-                <p>Смотреть фото</p>
-                <p>{parseFloat(item.sanaa, 5)}</p>
+                <span>{item.feedback}</span>
+                <div id="feedback-content-mainy-right-content-bottom-content">
+                  <p>Смотреть фото</p>
+                  <p>{parseFloat(item.sanaa, 5)}</p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <button id="add-feed-btn">asdd</button>
     </div>
   );
 }
